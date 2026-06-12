ba5e7410912a93f508d350327903c63615e15ca1
mishra <jisumishra4@gmail.com>
2026-06-12 23:29:43 +0530

    Learn The Code Of The Abstract Method

---
+++ oops.java/Abstract/Main.java
@@ -0,0 +1,8 @@
+package Abstract;
+
+public class Main {
+  public static void main(String[] args) {
+    Son son = new Son();
+    son.career(null);
+  }
+}
---
+++ oops.java/Abstract/Son.java
@@ -0,0 +1,5 @@
+package Abstract;
+
+public class Son extends parent {
+  
+}
