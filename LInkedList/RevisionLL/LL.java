0b77541cf154760bbd79d040c0178b1bc5394476
mishra <jisumishra4@gmail.com>
2026-07-27 23:45:54 +0530

    LL Implementations

---
+++ LInkedList/RevisionLL/LL.java
@@ -0,0 +1,25 @@
+package RevisionLL;
+
+public class LL {
+  public class Node {
+    private Node head;
+    private Node tail;
+    private int size;
+
+   public LL () {
+    this.size = 0;
+   }
+
+  }
+
+  private class Node {
+    private int value;
+    private Node next;
+
+    public Node(int value, Node next) {
+      this.value = value;
+      this.next = next;
+    }
+
+  }
+}
---
+++ LInkedList/RevisionLL/Main.java
@@ -0,0 +1,5 @@
+package RevisionLL;
+
+public class Main {
+
+}
